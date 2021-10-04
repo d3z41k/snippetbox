5950b9a39698666b3579436e58595afc28b1700b
Denis Zhokhov <d3z41k@gmail.com>
2021-10-04 13:01:34 +0200

    Add user authorization

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,15 +12,15 @@ func (app *application) routes() http.Handler {
 
 	r := mux.NewRouter()
 	r.Handle("/", dynamicMiddleware.ThenFunc(app.home)).Methods("GET")
-	r.Handle("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippetForm)).Methods("GET")
-	r.Handle("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet)).Methods("POST")
+	r.Handle("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippetForm)).Methods("GET")
+	r.Handle("/snippet/create", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.createSnippet)).Methods("POST")
 	r.Handle("/snippet/{id}", dynamicMiddleware.ThenFunc(app.showSnippet)).Methods("GET")
 
 	r.Handle("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm)).Methods("GET")
 	r.Handle("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser)).Methods("POST")
 	r.Handle("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm)).Methods("GET")
 	r.Handle("/user/login", dynamicMiddleware.ThenFunc(app.loginUser)).Methods("POST")
-	r.Handle("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser)).Methods("POST")
+	r.Handle("/user/logout", dynamicMiddleware.Append(app.requireAuthentication).ThenFunc(app.logoutUser)).Methods("POST")
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
