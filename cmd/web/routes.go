69e436cec318a7d1c4af6b21503e239a016a3e72
Denis Zhokhov <d3z41k@gmail.com>
2021-10-07 11:38:21 +0200

    Add CSRF protection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,7 +8,7 @@ import (
 
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	dynamicMiddleware := alice.New(app.session.Enable)
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf)
 
 	r := mux.NewRouter()
 	r.Handle("/", dynamicMiddleware.ThenFunc(app.home)).Methods("GET")
