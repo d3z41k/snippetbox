fb9f3763176894d9aa6b0375aa9c301021a54467
Denis Zhokhov <d3z41k@gmail.com>
2021-11-02 15:52:25 +0200

    Add HTML forms testing

--- pkg/models/mock/users.go
+++ pkg/models/mock/users.go
@@ -17,7 +17,7 @@ type UserModel struct{}
 
 func (m *UserModel) Insert(name, email, password string) error {
 	switch email {
-	case "dupe@email.io":
+	case "dupe@example.com":
 		return models.ErrDuplicateEmail
 	default:
 		return nil
